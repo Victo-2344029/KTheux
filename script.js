bdaf7c4f785e6a395c60bf19ae8bddd3e9f20a7b
Killian Theux <2344029@carrefour.cegepvicto.ca>
2024-12-04 14:27:37 -0500

    Version Finale

--- script.js
+++ script.js
@@ -9,10 +9,16 @@ class Question {
     }
   }
   let questions = [
-    new Question("Quelle méthode Javascript permet de filtrer les éléments d'un tableau", ["indexOf()", "map()", "filter()", "reduce()"], "filter()"),
-    new Question("Quelle méthode Javascript permet de vérifier si un élément figure dans un tableau", ["isNaN()","includes()", "findIndex()", "isOdd()"], "includes()"),
-    new Question("Quelle méthode transforme du JSON en un objet Javascript ?", ["JSON.parse()","JSON.stringify()", "JSON.object()", "JSON.toJS"], "JSON.parse()"),
-    new Question("Quel objet Javascript permet d'arrondir à l'entier le plus proche", ["Math.ceil()","Math.floor()", "Math.round()", "Math.random()"], "Math.round()")
+    new Question("Quelle est la capitale de la France ?", ["Madrid", "Berlin", "Paris", "Rome"], "Paris"),
+    new Question("Quel est l'élément chimique dont le symbole est 'O' ?", ["Or","Oxygène", "Osmium", "Ozone"], "Oxygène"),
+    new Question("Qui a écrit 'Les Misérables' ?", ["Victor Hugo","Albert Camus", "Marcel Proust", "Gustave Flaubert"], "Victor Hugo"),
+    new Question("Quelle est la plus grande planète de notre système solaire ?", ["Mars","Terre", "Jupiter", "Saturne"], "Jupiter"),
+    new Question("Quel est le plus long fleuve du monde ?", ["Amazone","Nil", "Yangtsé", "Mississippi"], "Amazone"),
+    new Question("Qui a peint le célèbre tableau La Nuit étoilée ?", ["Claude Monet", "Pablo Picasso", "Vincent van Gogh", "Edvard Munch"], "Vincent van Gogh"),
+    new Question("Quelle est la capitale du Canada ?", ["Toronto", "Vancouver", "Ottawa", "Montréal"], "Ottawa"),
+    new Question("En quelle année l'homme a-t-il marché pour la première fois sur la Lune ?", ["1965", "1969", "1972", "1980"], "1969"),
+    new Question("Quel est le plus grand océan de la planète ?", ["Atlantique", "Indien", "Pacifique", "Arctique"], "Pacifique"),
+    new Question("Quelle est la langue officielle du Brésil ?", ["Espagnol", "Portugais", "Italien", "Anglais"], "Portugais")
   ];
   
   class Quiz {
@@ -34,6 +40,11 @@ class Question {
       return this.currentQuestionIndex >= this.questions.length;
     }
   }
+
+  const boutonRe = document.querySelector('#bouton');
+  const boutonPlusAfficher = document.querySelector('#plusAfficher');
+  const fini = document.querySelector('#fini');
+  const debutQuiz = document.querySelector('#quiz');
   
   const display = {
     elementShown: function(id, text) {
@@ -44,7 +55,10 @@ class Question {
       endQuizHTML = `
         <h1>Quiz terminé !</h1>
         <h3> Votre score est de : ${quiz.score} / ${quiz.questions.length}</h3>`;
-      this.elementShown("quiz", endQuizHTML);
+      this.elementShown("fini", endQuizHTML);
+      debutQuiz.classList.add('hidden');
+      fini.classList.remove('hidden');
+      boutonRe.classList.remove('hidden');
     },
     question: function() {
       this.elementShown("question", quiz.getCurrentQuestion().text);
@@ -83,3 +97,47 @@ class Question {
   // Create Quiz
   let quiz = new Quiz(questions);
   quizApp();
+
+let boolPlusAfficher = localStorage.getItem('AfficherDialog');
+console.log(boolPlusAfficher);
+
+/**
+ * Afficher le dialog au chargement de la page, sauf si le boutton 'Ne plus afficher' à été cliqué.
+ */
+window.onload = function() {
+  if(boolPlusAfficher == 'false'){
+    document.getElementById('modal').style.display = 'none';
+  }
+  else{
+    document.getElementById('modal').style.display = 'block';
+  }
+};
+
+/**
+ * Fermer la fenêtre modale lorsque le bouton 'Fermer la fenêtre' est cliqué.
+ */
+document.getElementById('close-btn').onclick = function() {
+  document.getElementById('modal').style.display = 'none';
+};
+
+/**
+ * Fermer la fenêtre modale lorsque le bouton 'Ne plus afficher' est cliqué, et mettre la valeur de 'AfficherDialog' à false afin de ne plus l'afficher par la suite.
+ */
+document.getElementById('plusAfficher').onclick = function(){
+  document.getElementById('modal').style.display = 'none';
+  localStorage.setItem('AfficherDialog','false');
+}
+
+/**
+ * Redémarrer le quiz et réinitialiser le score lorsque le bouton 'Recommencer' est cliqué.
+ */
+boutonRe.addEventListener('click', function(){
+  // Create Quiz
+  debutQuiz.classList.remove('hidden');
+  fini.classList.add('hidden');
+  boutonRe.classList.add('hidden');
+
+  quiz = new Quiz(questions);
+  quiz.currentQuestionIndex = 0;
+  quizApp();
+})
